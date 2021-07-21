24487ae8562575e1724b0c3a35a43569c4c0202e
renren0530 <zoo.zoo.ren@gmail.com>
2021-07-21 19:06:14 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  has_many :rooms, thorough: :room_users
   has_many :room_users
+  has_many :rooms, through: :room_users
+ 
 
 
 end
