2a78c1d6fed2b6242e526afc376caf2fa9c9f236
renren0530 <zoo.zoo.ren@gmail.com>
2021-07-23 09:19:00 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,8 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
     @message = Message.new 
+    @messages = @room.messages.includes(:user)
+
   end
 
   def create
@@ -12,6 +14,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
      else
+      @messages = @room.messages.includes(:user)
       reder :index
      end
   end
